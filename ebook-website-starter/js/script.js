65e13741461001dba73b6effc190ca87e4f4ff35
VishalGRV <49804433+VishalGRV@users.noreply.github.com>
2024-10-11 15:42:14 +0530

    Initial commit

---
+++ ebook-website-starter/js/script.js
@@ -0,0 +1,20 @@
+// Add custom JavaScript here
+
+function userScroll(){
+    var navbar = document.querySelector('.navbar');
+    window.addEventListener('scroll', function(){
+        if(window.scrollY > 50){
+            navbar.classList.add('bg-dark');
+            navbar.classList.add('navbar-sticky');
+
+            
+        }
+        else{
+            navbar.classList.remove('bg-dark');
+            navbar.classList.remove('navbar-sticky');
+
+        }
+    });
+}
+
+document.addEventListener('DOMContentLoaded', userScroll);
